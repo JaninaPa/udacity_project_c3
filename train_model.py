8dc8a41bcf7ab1e4289b77475cfdc1e721b90fe0
JaninaPa <janina_patzer_99@web.de>
2022-03-13 22:30:13 +0100

    Added training of lr model

--- train_model.py
+++ train_model.py
@@ -1,12 +1,18 @@
 # Script to train machine learning model.
 
+from cv2 import mean
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import pickle
+from ml.data import process_data
+from ml.model import *
 
-# Add the necessary imports for the starter code.
+# Load in  data
 
-# Add code to load in the data.
+data = pd.read_csv("census_cleaned.csv")
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Split data in train and test set
+
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -19,10 +25,42 @@ cat_features = [
     "sex",
     "native-country",
 ]
+# Process data
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    train, 
+    categorical_features=cat_features, 
+    label="salary", 
+    training=True
+)
+x_test, y_test, encoder_test, lb_test = process_data(
+    test,
+    categorical_features=cat_features,
+    encoder=encoder,
+    lb=lb,
+    label="salary",
+    training=False,
 )
 
-# Proces the test data with the process_data function.
+# Fit classifier
 
-# Train and save a model.
+model = train_model(X_train,y_train)
+predictions = inference(model,x_test)
+
+# Evaluate model
+
+mean_acc = model.score(x_test,y_test)
+precision, recall, fbeta = compute_model_metrics(y_test, predictions)
+
+print("###### Performance ######")
+print("Mean Accuracy: ", mean_acc)
+print("Precision:     ", precision)
+print("Recall:        ", recall)
+print("Fbeta:         ", fbeta)
+
+# Save model
+
+model_file = 'census_lr_model.sav'
+encoder_file = 'census_lr_enc.sav'
+
+pickle.dump(model, open(model_file, 'wb'))
+pickle.dump(model, open(encoder_file, 'wb'))
