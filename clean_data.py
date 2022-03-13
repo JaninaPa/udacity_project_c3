533a576936a29c94aa002e62712085aebdc4980e
JaninaPa <janina_patzer_99@web.de>
2022-03-14 00:13:04 +0100

    Added removal of question marks

--- clean_data.py
+++ clean_data.py
@@ -13,4 +13,20 @@ print(df.columns)
 df.columns = df.columns.str.strip()
 print(df.columns)
 
+# The slice testing showed, that some entries contain question marks.
+# Removal of '?'
+list_cat_cols = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+for col in list_cat_cols:
+    df = df.drop(df[df[col].str.contains("?", regex=False)].index)
+print(df.shape)
+
 df.to_csv("census_cleaned.csv")
