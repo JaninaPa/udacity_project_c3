e930371b0a9cf5a5c85f5c8f5d500929f430875a
JaninaPa <janina_patzer_99@web.de>
2022-03-14 12:02:36 +0100

    Fixed error in df creation

--- tests/test_training.py
+++ tests/test_training.py
@@ -31,7 +31,7 @@ def data():
         [50, "Self-emp-not-inc", 83311, "Bachelors", 13, "Married-civ-spouse",  "Exec-managerial", "Husband", "White", "Male", 0, 0, 13,  "United-States", " <=50K"],
         [38, "Private", 215646, "HS-grad", 9,  "Divorced",  "Handlers-cleaners",  "Not-in-family", "White", "Male", 0, 0, 40,  "United-States", " <=50K"],
         [53, "Private", 234721, "11th", 7, "Married-civ-spouse",  "Handlers-cleaners", "Husband", "Black", "Male", 0, 0, 40,  "United-States", " <=50K"],
-        [28, "Private", 338409, "Bachelors", 13, "Married-civ-spouse",  "Prof-specialty", "Wife", "Black", "Female", 0, 0, 40,  "Cuba", " <=50K]"],
+        [28, "Private", 338409, "Bachelors", 13, "Married-civ-spouse",  "Prof-specialty", "Wife", "Black", "Female", 0, 0, 40,  "Cuba", " <=50K"],
         [37, "Private", 284582, "Masters", 14, "Married-civ-spouse",  "Exec-managerial", "Wife", "White", "Female", 0, 0, 40,  "United-States", " <=50K"],
         [49, "Private", 160187, "9th", 5, "Married-spouse-absent",  "Other-service",  "Not-in-family", "Black", "Female", 0, 0, 16,  "Jamaica", " <=50K"],
         [52, "Self-emp-not-inc", 209642, "HS-grad", 9, "Married-civ-spouse",  "Exec-managerial", "Husband", "White", "Male", 0, 0, 45,  "United-States", " >50K"],
@@ -82,7 +82,7 @@ def test_train_model(data):
     # Split data in train and test set
 
     train, test = train_test_split(data, test_size=0.20)
-
+    print(train.head(10))
     cat_features = [
         "workclass",
         "education",
@@ -102,7 +102,6 @@ def test_train_model(data):
     )
 
     # Fit classifier
-
     model = train_model(X_train,y_train)
 
     assert model
@@ -139,7 +138,6 @@ def test_inference(data):
         label="salary",
         training=False,
     )
-
     # Fit classifier
 
     model = train_model(X_train,y_train)
@@ -154,4 +152,3 @@ def test_inference(data):
     assert isinstance(precision,float)
     assert isinstance(recall,float)
     assert isinstance(fbeta,float)
-
