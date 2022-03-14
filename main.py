10b0df56bb311637b251d9e3655947df940ec6a5
JaninaPa <janina_patzer_99@web.de>
2022-03-14 16:13:31 +0100

    Fixed example error

--- main.py
+++ main.py
@@ -32,20 +32,20 @@ class RequestBody(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "age":42, 
-                "workclass":"Private",
-                "fnlgt": 159449, 
-                "education":"Bachelors", 
-                "education_num":13, 
-                "marital_status":"Married-civ-spouse",  
-                "occupation":"Exec-managerial",
-                "relationship":"Husband", 
-                "race":"White", 
-                "sex":"Male", 
-                "capital_gain":5178, 
-                "capital_loss":0, 
-                "hours_per_week": 40,  
-                "native_country":"United-States"
+                "age":[42], 
+                "workclass":["Private"],
+                "fnlgt": [159449], 
+                "education":["Bachelors"], 
+                "education_num":[13], 
+                "marital_status":["Married-civ-spouse"],  
+                "occupation":["Exec-managerial"],
+                "relationship":["Husband"], 
+                "race":["White"], 
+                "sex":["Male"], 
+                "capital_gain":[5178], 
+                "capital_loss":[0], 
+                "hours_per_week": [40],  
+                "native_country":["United-States"]
             }
         }
 
