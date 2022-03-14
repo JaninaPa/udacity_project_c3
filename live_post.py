f3d502ebfb49cab6acb05d0054ec53a902c0621f
JaninaPa <janina_patzer_99@web.de>
2022-03-14 16:13:08 +0100

    Initial commit of live api request

---
+++ live_post.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+post_data = {
+    "age": ["19"],
+    "workclass": ["Private"],
+    "fnlgt": ["301606"],
+    "education": ["Some-college"],
+    "education-num": ["10"],
+    "marital-status": ["Never-married"],
+    "occupation": ["Other-service"],
+    "relationship": ["Own-child"],
+    "race": ["Black"],
+    "sex": ["Male"],
+    "capital-gain": ["0"],
+    "capital-loss": ["0"],
+    "hours-per-week": ["35"],
+    "native-country": ["United-States"],
+}
+response = requests.post('https://udacity-project-c3.herokuapp.com/model_inference/', 
+                        data=json.dumps(post_data))
+if response.status_code == 200:
+    print("Request successful. Status Code: 200")
+    print("Response body:")
+    print(response.json())
